691573bf3199cc624d759bf27a5834f668c3599d
Andrew Peterson <andrewp2@umbc.edu>
2019-01-25 15:39:29 -0500

    added request for username

--- cluck2.go
+++ cluck2.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net"
 	"os"
+	"time"
 )
 
 /*
@@ -16,23 +17,42 @@ import (
 
  ConnPort - We are using port 8888
 
- ConnType - This client uses tcp. can be udp as well.
+ ConnType - This client uses tcp.
+
+ byteArraySize - The standard array size
+
+ bufferSize - 2^16
+
+ requestMLength - When the request doesn't contain a message, length of 0
+
+ MOTDCode - Type uint16. The code is 2.
+
+ registerCode - Type uint16. The code is 100.
 */
 const (
-	ConnIPEn = "130.85.70.132"
-	ConnPort = "8888"
-	ConnType = "tcp"
+	ConnIPEn              = "130.85.70.132"
+	ConnPort              = "8888"
+	ConnType              = "tcp"
+	byteArraySize         = 2
+	bufferSize            = 65536
+	requestMLength uint16 = 0
+	MOTDCode       uint16 = 2
+	registerCode   uint16 = 100
 )
 
 func main() {
+	// TODO an option for the user to pick if they want the MOTD or not?
 	tcpAddr, err := net.ResolveTCPAddr(ConnType, ConnIPEn+":"+ConnPort)
 	conn, err := net.DialTCP(ConnType, nil, tcpAddr) // Connect to server with tcp and handle any errors
 
+	fmt.Println("Connecting to the server now.")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer conn.Close()
 	go printOutput(conn)
+	registerUser(conn)
+	time.Sleep(1 * time.Millisecond)
 	askForMotd(conn)
 	for {
 		writeInput(conn)
@@ -51,14 +71,31 @@ func writeInput(conn *net.TCPConn) {
 //
 // Currently, the code for the MOTD is 2
 func askForMotd(conn *net.TCPConn) {
-	var mCode uint16 = 2
-	arraySize := 2
-	mCodeArray := make([]byte, arraySize) // Creates a byte type array of size 2
-	writer := bufio.NewWriter(conn)       // Creates a writer to right to the server
+	mCodeArray := make([]byte, byteArraySize)   // Creates a byte type array of size 2 for the message type
+	mLengthArray := make([]byte, byteArraySize) // Creates a byte type array of size 0 for the message length
 
-	binary.BigEndian.PutUint16(mCodeArray, mCode) // Converts bytes to size 16 unsigned Int
-	writer.Write(mCodeArray)
-	fmt.Println("done")
+	fmt.Println("Getting the MOTD")
+	binary.BigEndian.PutUint16(mCodeArray, MOTDCode)         // Converts bytes to size 16 unsigned Int, saves to the code array
+	binary.BigEndian.PutUint16(mLengthArray, requestMLength) // Converts bytes to size 16 unsigned Int, saves to the length array
+	conn.Write(append(mCodeArray, mLengthArray...))          // Writes the bytes to the server
+}
+
+// Sends the server a request to register a user
+func registerUser(conn *net.TCPConn) {
+	rCodeArray := make([]byte, byteArraySize) // Creates a byte type array of size 2
+	fmt.Println("Enter a username:")
+	reader := bufio.NewReader(os.Stdin)
+	text, _ := reader.ReadString('\n')                  // Takes in the user's input
+	registerMLength := uint16(len(text) - 1)            // Takes the length of the user's input
+	registerMLengthArray := make([]byte, byteArraySize) // creates an array the size of the length portion of the header
+	username := []byte(text)                            // Creates the username byte array with the bytes of the input string
+
+	binary.BigEndian.PutUint16(rCodeArray, registerCode)              // Converts bytes to size 16 unsigned Int
+	binary.BigEndian.PutUint16(registerMLengthArray, registerMLength) // Converts bytes to size 16 unsigned Int, saves to the length array
+	x := []byte{}                                                     // Creating a temp array
+	x = append(rCodeArray, registerMLengthArray...)                   // Appends the header arrays to the temp array
+	//fmt.Println(append(x, username...))
+	conn.Write(append(x, username...)) // Appends the message array to the temp array. Writes the bytes to the server
 }
 
 // Creates and receives the buffer from the server
@@ -69,8 +106,7 @@ func askForMotd(conn *net.TCPConn) {
 //
 // <unsigned_short: message_code> <unsigned_short: message_length> <char* string (not null terminated)>
 func printOutput(conn *net.TCPConn) {
-	bufferSize := 65536                // 2^16
-	buffer := make([]byte, bufferSize) // Creates the buffer5536 ==
+	buffer := make([]byte, bufferSize) // Creates the buffer
 	inMessage := bufio.NewReader(conn) // Creates a new reader buffer for the connection
 
 	// Endless loop to print the received data to the terminal
@@ -80,7 +116,7 @@ func printOutput(conn *net.TCPConn) {
 		mLength := buffer[2:4]           // Message length
 		mString := buffer[4:n]           // Message string
 
-		getPacketMessage(mString)
+		fmt.Println(getPacketMessage(mString))
 		getPacketCode(mCode)
 		getPacketLength(mLength)
 		if err != nil {
@@ -91,19 +127,19 @@ func printOutput(conn *net.TCPConn) {
 }
 
 // Takes the bytes and converts them to string
-func getPacketMessage(mString []byte) {
+func getPacketMessage(mString []byte) string {
 	s := string(mString[:])
-	fmt.Println(s)
+	return s
 }
 
 // Takes the bytes and converts them to an unsigned short (uint16)
-func getPacketCode(mCode []byte) {
+func getPacketCode(mCode []byte) uint16 {
 	dCode := binary.BigEndian.Uint16(mCode)
-	fmt.Println(dCode)
+	return dCode
 }
 
 // Takes the bytes and converts them to an unsigned short (uint16)
-func getPacketLength(mLength []byte) {
+func getPacketLength(mLength []byte) uint16 {
 	dLength := binary.BigEndian.Uint16(mLength)
-	fmt.Println(dLength)
+	return dLength
 }
