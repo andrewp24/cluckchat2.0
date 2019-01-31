35e0a793274fe7616f9d41a10afb8cad8fabc1d4
Andrew Peterson <andrewp2@umbc.edu>
2019-01-31 17:30:39 -0500

    added locking, getPackage function, whoAmI

--- cluck2.go
+++ cluck2.go
@@ -7,7 +7,7 @@ import (
 	"log"
 	"net"
 	"os"
-	"time"
+	"sync"
 )
 
 /*
@@ -27,7 +27,11 @@ import (
 
  MOTDCode - Type uint16. The code is 2.
 
+ whoAmICode - Type uint16. The code is 110
+
  registerCode - Type uint16. The code is 100.
+
+ failureCode - Type uint16. The code is 11.
 */
 const (
 	ConnIPEn              = "130.85.70.132"
@@ -37,8 +41,12 @@ const (
 	bufferSize            = 65536
 	requestMLength uint16 = 0
 	MOTDCode       uint16 = 2
+	whoAmICode     uint16 = 110
 	registerCode   uint16 = 100
+	failureCode    uint16 = 11
 )
+
+var m = sync.Mutex{}
 
 func main() {
 	// TODO an option for the user to pick if they want the MOTD or not?
@@ -50,13 +58,12 @@ func main() {
 		log.Fatal(err)
 	}
 	defer conn.Close()
-	// look at this: https://youtu.be/icbFEmh7Ym0?t=708
 	go printOutput(conn)
 	registerUser(conn)
-	time.Sleep(1 * time.Millisecond)
-	askForMotd(conn)
+	whoAmI(conn)
 	for {
-		writeInput(conn)
+		//	writeInput(conn)
+
 	}
 }
 
@@ -65,7 +72,29 @@ func writeInput(conn *net.TCPConn) {
 	reader := bufio.NewReader(os.Stdin)
 	text, _ := reader.ReadString('\n')
 
+	m.Lock()
 	fmt.Fprintf(conn, text+"\n")
+	m.Unlock()
+}
+
+func whoAmI(conn *net.TCPConn) {
+	wCodeArray := make([]byte, byteArraySize)   // Creates a byte type array of size 2 for the message type
+	wLengthArray := make([]byte, byteArraySize) // Creates a byte type array of size 0 for the message length
+
+	//fmt.Println("Getting the MOTD")
+	binary.BigEndian.PutUint16(wCodeArray, whoAmICode)       // Converts bytes to size 16 unsigned Int, saves to the code array
+	binary.BigEndian.PutUint16(wLengthArray, requestMLength) // Converts bytes to size 16 unsigned Int, saves to the length array
+	m.Lock()
+	conn.Write(append(wCodeArray, wLengthArray...)) // Writes the bytes to the server
+	code, _, message := getPackage(conn)
+
+	if code == failureCode {
+		fmt.Println("You haven't been registered.")
+	} else {
+		fmt.Println("You are: " + message)
+	}
+	m.Unlock()
+
 }
 
 // Requests the message of the day from the server
@@ -75,13 +104,17 @@ func askForMotd(conn *net.TCPConn) {
 	mCodeArray := make([]byte, byteArraySize)   // Creates a byte type array of size 2 for the message type
 	mLengthArray := make([]byte, byteArraySize) // Creates a byte type array of size 0 for the message length
 
-	fmt.Println("Getting the MOTD")
+	//fmt.Println("Getting the MOTD")
 	binary.BigEndian.PutUint16(mCodeArray, MOTDCode)         // Converts bytes to size 16 unsigned Int, saves to the code array
 	binary.BigEndian.PutUint16(mLengthArray, requestMLength) // Converts bytes to size 16 unsigned Int, saves to the length array
 	conn.Write(append(mCodeArray, mLengthArray...))          // Writes the bytes to the server
+	_, _, message := getPackage(conn)                        // Retrieves the message of the package
+	fmt.Println(message)
 }
 
 // Sends the server a request to register a user
+//
+// If the user successfully registers, the MOTD is requested.
 func registerUser(conn *net.TCPConn) {
 	rCodeArray := make([]byte, byteArraySize) // Creates a byte type array of size 2
 	fmt.Println("Enter a username:")
@@ -96,10 +129,32 @@ func registerUser(conn *net.TCPConn) {
 	binary.BigEndian.PutUint16(registerMLengthArray, registerMLength) // Converts bytes to size 16 unsigned Int, saves to the length array
 	x := []byte{}                                                     // Creating a temp array
 	x = append(rCodeArray, registerMLengthArray...)                   // Appends the header arrays to the temp array
-	conn.Write(append(x, username...))                                // Appends the message array to the temp array. Writes the bytes to the server
+	m.Lock()
+	conn.Write(append(x, username...)) // Appends the message array to the temp array. Writes the bytes to the server
+	code, _, _ := getPackage(conn)     // Retreives the code of the package
+	if code == failureCode {
+		println("Failed to register the user.")
+	} else {
+		askForMotd(conn)
+	}
+	m.Unlock()
+}
 
-	// get the output here
-
+// getPackage takes the connection and returns the code, length, and message of the package
+func getPackage(conn *net.TCPConn) (uint16, uint16, string) {
+	buffer := make([]byte, bufferSize)
+	recMessage := bufio.NewReader(conn)
+	n, err := recMessage.Read(buffer) // n is the number of bytes in the buffer
+	mCode := buffer[0:2]              // Message code
+	mLength := buffer[2:4]            // Message length
+	mString := buffer[4:n]            // Message string
+	packetCode := getPacketCode(mCode)
+	packetLength := getPacketLength(mLength)
+	packetMessage := getPacketMessage(mString)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return packetCode, packetLength, packetMessage
 }
 
 // Creates and receives the buffer from the server
@@ -109,29 +164,28 @@ func registerUser(conn *net.TCPConn) {
 // The packet sent from the server is in this format:
 //
 // <unsigned_short: message_code> <unsigned_short: message_length> <char* string (not null terminated)>
-// look at how to do non blocking !!!!!!!!!!!!!
+// look at how to do locks
 func printOutput(conn *net.TCPConn) {
 	buffer := make([]byte, bufferSize) // Creates the buffer
 	inMessage := bufio.NewReader(conn) // Creates a new reader buffer for the connection
 
 	// Endless loop to print the received data to the terminal
 	for {
-		n, err := inMessage.Read(buffer) // n is the number of bytes in the buffer
-		mCode := buffer[0:2]             // Message code
-		mLength := buffer[2:4]           // Message length
-		mString := buffer[4:n]           // Message string
-		packetCode := getPacketCode(mCode)
+		m.Lock()
+		if inMessage.Buffered() > 0 {
+			fmt.Println(inMessage.Buffered())
+			_, err := inMessage.Read(buffer) // n is the number of bytes in the buffer
+			code, _, message := getPackage(conn)
+			m.Unlock()
+			if err != nil {
+				fmt.Println(err)
+				break
+			}
+			fmt.Println(code, message)
+		} else {
+			m.Unlock()
+		}
 
-		getPacketMessage(mString)
-		getPacketLength(mLength)
-		if err != nil {
-			fmt.Println(err)
-			break
-		}
-		if packetCode == 11 {
-			fmt.Println("Username unsuccessful")
-			continue
-		}
 	}
 }
 
